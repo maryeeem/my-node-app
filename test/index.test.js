cef6bec5d211730ef6b2e7079cb922d171a1ac68
maryeeem <cherif.maryem@esprit.tn>
2025-01-21 16:51:41 +0100

    Add initial test for array indexOf method

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+onst assert = require('chai').assert;
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+
+        it('should return the index when the value is present', function() {
+            assert.equal([1, 2, 3].indexOf(2), 1);
+        });
+    });
+});
